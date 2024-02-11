110a5498aed428bbf2f141cc854a33eccf24211d
Ankush Kumar <imankush1010@gmail.com>
2024-02-11 23:40:38 +0530

    Added vercel speed test

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const App = () => {
   return (
